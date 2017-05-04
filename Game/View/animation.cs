884d10b2320e14afc79d04bb36be74d107326a76
GearShift24 <gearshift24@gmail.com>
2017-05-04 09:40:22 -0600

    started and made game

---
+++ Game/View/animation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Animation.cs
+//Using declarations
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game
+	{
+	public class animation
+		{
+		public animation()
+			{
+			}
+		}
+	}
